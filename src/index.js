62d7b130f6653ccbb9206fe96307ce21653f48a4
Aleksandr Sarkisov <oleksandr.sarkisov@onereach.com>
2024-10-10 12:27:51 +0300

    feat: add routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { Home, Resume } from './components';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/:name",
+    element: <Resume />,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
